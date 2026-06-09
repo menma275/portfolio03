f17dc4e9408e4ea5b8449f4548a56179e0654b7e
menma275 <ohsusie9@gmail.com>
2026-06-09 15:18:33 +0900

    Headerアニメーションの削除, Profileの拡充

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -16,6 +16,19 @@ export default function ProfilePage() {
       <TabNavigation activeTab="profile" />
       <div className="transition-all duration-300">
         <div className="flex flex-col gap-16">
+          <FadeIn>
+            <p>
+              2002年東京都生まれ <br />
+              WebやGenerativeArtを中心に、テクノロジーを使ったデザイン・アート表現を探求。
+              <br />
+              情報系高専在学時にGenerative
+              Artの制作を始め、ブロックチェーン技術を使用したNFTとして作品を発表、多くの人々からのリアクションを得たことをきっかけにテクノロジーによる作品制作に強い関心を持つ。
+              <br />
+              多様な技術領域に関心を持ち、核となるアイデアを大切にしながら、アルゴリズムによって動作する論理性と感性的な心地よさの両立を追求して作品制作を行う。
+              <br />
+              ハッカソンや作品展示などに積極的に参加しながら、多くの人々と意見やアイデアを交換することで常に新しい表現手法を模索している。
+            </p>
+          </FadeIn>
           <FadeIn delay={0.1}>
             <Education />
           </FadeIn>
--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -1,37 +1,12 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { socials } from "@/data";
 import { ExternalLink } from "./ExternalLink";
 
 export const SocialLinks: React.FC = () => {
-  const [isScrolled, setIsScrolled] = useState(false);
-
-  useEffect(() => {
-    const mainElement = document.querySelector("main");
-    if (!mainElement) return;
-
-    const handleScroll = () => {
-      // Hide links when scrolled more than 20px
-      if (mainElement.scrollTop > 20) {
-        setIsScrolled(true);
-      } else {
-        setIsScrolled(false);
-      }
-    };
-
-    mainElement.addEventListener("scroll", handleScroll);
-    return () => mainElement.removeEventListener("scroll", handleScroll);
-  }, []);
-
   return (
-    <div
-      className={`flex flex-wrap md:flex-col gap-2 transition-all duration-500 overflow-hidden ${
-        isScrolled
-          ? "max-md:opacity-0 max-md:max-h-0 max-md:mt-[-8px] pointer-events-none"
-          : "max-md:opacity-100 max-md:max-h-40"
-      }`}
-    >
+    <div className="flex flex-wrap md:flex-col gap-2">
       {socials.map((social) => (
         <ExternalLink key={social.label} href={social.href}>
           {social.label}
