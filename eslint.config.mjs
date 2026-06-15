1aa22520307991980d8238db28746dc0d2455ed8
menma275 <ohsusie9@gmail.com>
2026-06-16 00:04:38 +0900

    Fix: Exclude .pnpm-store from Prettier and ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,6 +8,8 @@ const eslintConfig = defineConfig([
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
+    "node_modules/**",
+    ".pnpm-store/**",
     ".next/**",
     "out/**",
     "build/**",
