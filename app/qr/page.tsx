bb1fece0261c3a550f6761ad07c7460eb5dc31d0
menma275 <ohsusie9@gmail.com>
2026-06-16 01:27:22 +0900

    QRページの追加

---
+++ app/qr/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function QRPage() {
+  return (
+    <div className="h-full flex flex-col items-center justify-center p-4">
+      <div className="relative aspect-square w-full max-w-md">
+        <Image
+          src="/qr.svg"
+          alt="QR Code"
+          fill
+          className="object-contain"
+          priority
+        />
+      </div>
+    </div>
+  );
+}
