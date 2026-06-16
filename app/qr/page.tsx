60a356c77103a8bc675c0871c663a75026070bea
menma275 <ohsusie9@gmail.com>
2026-06-16 09:58:33 +0900

    QRページの修正

--- app/qr/page.tsx
+++ app/qr/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function QRPage() {
   return (
     <div className="h-full flex flex-col items-center justify-center p-4">
-      <div className="relative aspect-square w-full max-w-md">
+      <div className="relative aspect-square w-full max-w-48">
         <Image
           src="/qr.svg"
           alt="QR Code"
