4cc5057fae1b5626cb0115fead5d00a6a27380e2
menma275 <ohsusie9@gmail.com>
2026-06-23 17:00:21 +0900

    ImageModalのbackdropを濃く

--- components/ImageModal.tsx
+++ components/ImageModal.tsx
@@ -74,7 +74,7 @@ export function ImageModal({
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
-        className="absolute inset-0 backdrop-blur-sm"
+        className="absolute inset-0 backdrop-blur-lg"
         onClick={handleClose}
       />
 
