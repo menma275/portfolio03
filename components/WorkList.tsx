f23c9663d3d913938e336106cf63229ae0c4a314
menma275 <ohsusie9@gmail.com>
2026-07-01 11:16:02 +0900

    WorkListのgrid数を修正

--- components/WorkList.tsx
+++ components/WorkList.tsx
@@ -98,7 +98,7 @@ const WorkListContent: React.FC = () => {
       {/* Work Cards Grid */}
       <div
         key={selectedCategory}
-        className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4"
+        className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4"
       >
         {filteredWorks.map((work, index) => (
           <FadeIn key={work.id} delay={index * 0.05} className="h-full">
