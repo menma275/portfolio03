1248d62d7ba3f9e560a80ca38e494b4a3b2143ca
menma275 <ohsusie9@gmail.com>
2026-08-28 15:52:44 +0900

    style: position social links at bottom on md breakpoint

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,17 +4,19 @@ import Link from "next/link";
 
 export const Header: React.FC = () => {
   return (
-    <div className="flex flex-col gap-6">
-      <div className="hidden md:block">
-        <HeroVideo videoSrc="/river.mp4" imageSrc="/river.jpg" />
+    <div className="flex flex-col gap-6 md:h-full md:justify-between">
+      <div className="flex flex-col gap-6">
+        <div className="hidden md:block">
+          <HeroVideo videoSrc="/river.mp4" imageSrc="/river.jpg" />
+        </div>
+        <div className="flex flex-col gap-2">
+          <Link href="/" className="hover:opacity-70 transition-opacity">
+            <h1 className="text-fg-primary">Kusuke SAKAMURA / 坂村 空介</h1>
+          </Link>
+          <p className="text-fg-secondary">go with the flow, like water.</p>
+        </div>
       </div>
-      <div className="flex flex-col gap-2">
-        <Link href="/" className="hover:opacity-70 transition-opacity">
-          <h1 className="text-fg-primary">Kusuke SAKAMURA / 坂村 空介</h1>
-        </Link>
-        <p className="text-fg-secondary">go with the flow, like water.</p>
-        <SocialLinks />
-      </div>
+      <SocialLinks />
     </div>
   );
 };
